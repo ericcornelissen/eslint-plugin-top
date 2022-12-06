8c43183eebea3bf6b0efef3938bbcb6651a839eb
Eric Cornelissen <ericornelissen@gmail.com>
2022-12-06 23:14:04 +0100

    Avoid repeating tests for each ESLint version in compatibility tests

--- tests/compat/main.test.ts
+++ tests/compat/main.test.ts
@@ -4,90 +4,66 @@ import * as path from 'path';
 
 import * as snapshots from './snapshots';
 
+const eslintVersions: ReadonlyArray<number> = [6, 7, 8];
+
 describe('compatibility', function () {
   describe('without violations', function () {
     const snapshot = snapshots.noViolations;
 
-    it('v6', function () {
-      const {exitCode, stdout} = runEslint(6, snapshot.inp);
-      assert.equal(exitCode, 0);
-      assert.equal(stdout, snapshot.out);
-    });
-
-    it('v7', function () {
-      const {exitCode, stdout} = runEslint(7, snapshot.inp);
-      assert.equal(exitCode, 0);
-      assert.equal(stdout, snapshot.out);
-    });
-
-    it('v8', function () {
-      const {exitCode, stdout} = runEslint(8, snapshot.inp);
-      assert.equal(exitCode, 0);
-      assert.equal(stdout, snapshot.out);
-    });
+    for (const eslintVersion of eslintVersions) {
+      it(`v${eslintVersion}`, function () {
+        const {exitCode, stdout} = runEslint(eslintVersion, snapshot.inp);
+        assert.equal(exitCode, 0);
+        assert.equal(stdout, snapshot.out);
+      });
+    }
   });
 
   describe('with top-level-variable violation', function () {
     const snapshot = snapshots.noTopLevelVariablesViolation;
 
-    it('v6', function () {
-      const {exitCode, stdout} = runEslint(6, snapshot.inp);
-      assert.equal(exitCode, 1);
-      assert.equal(stdout, snapshot.out);
-    });
-
-    it('v7', function () {
-      const {exitCode, stdout} = runEslint(7, snapshot.inp);
-      assert.equal(exitCode, 1);
-      assert.equal(stdout, snapshot.out);
-    });
-
-    it('v8', function () {
-      const {exitCode, stdout} = runEslint(8, snapshot.inp);
-      assert.equal(exitCode, 1);
-      assert.equal(stdout, snapshot.out);
-    });
+    for (const eslintVersion of eslintVersions) {
+      it(`v${eslintVersion}`, function () {
+        const {exitCode, stdout} = runEslint(eslintVersion, snapshot.inp);
+        assert.equal(exitCode, 1);
+        assert.equal(stdout, snapshot.out);
+      });
+    }
   });
 
   describe('with top-level-side-effect violation', function () {
     const snapshot = snapshots.noTopLevelSideEffectsViolation;
 
-    it('v6', function () {
-      const {exitCode, stdout} = runEslint(6, snapshot.inp);
-      assert.equal(exitCode, 1);
-      assert.equal(stdout, snapshot.out);
-    });
-
-    it('v7', function () {
-      const {exitCode, stdout} = runEslint(7, snapshot.inp);
-      assert.equal(exitCode, 1);
-      assert.equal(stdout, snapshot.out);
-    });
-
-    it('v8', function () {
-      const {exitCode, stdout} = runEslint(8, snapshot.inp);
-      assert.equal(exitCode, 1);
-      assert.equal(stdout, snapshot.out);
-    });
+    for (const eslintVersion of eslintVersions) {
+      it(`v${eslintVersion}`, function () {
+        const {exitCode, stdout} = runEslint(eslintVersion, snapshot.inp);
+        assert.equal(exitCode, 1);
+        assert.equal(stdout, snapshot.out);
+      });
+    }
   });
 });
 
 function runEslint(
-  version: 6 | 7 | 8,
+  version: number,
   snippet: string
 ): {exitCode: number | null; stdout: string} {
   const projectRoot = path.resolve('.');
   const nodeModules = path.resolve(projectRoot, 'node_modules');
-  const eslintClis = {
+  const eslintCli = {
     6: path.resolve(nodeModules, 'eslint-v6', 'bin', 'eslint.js'),
     7: path.resolve(nodeModules, 'eslint-v7', 'bin', 'eslint.js'),
     8: path.resolve(nodeModules, 'eslint-v8', 'bin', 'eslint.js')
-  };
+  }[version];
+
+  if (!eslintCli) {
+    throw new Error(`Unknown ESLint version ${version}`);
+  }
 
   const {status, stdout} = cp.spawnSync(
     'node',
     [
-      eslintClis[version],
+      eslintCli,
       // Avoid interference from a local ESLint configuration file
       '--no-eslintrc',
       // Lint from stdin
