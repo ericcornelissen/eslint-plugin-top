6825206432a7d508ff75f03b5782ad66b9354a5f
Eric Cornelissen <ericornelissen@gmail.com>
2024-12-16 09:53:22 +0100

    Enable new `json/top-level-interop` rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -117,7 +117,8 @@ export default [
       'json/no-duplicate-keys': 'error',
       'json/no-empty-keys': 'error',
       'json/no-unnormalized-keys': 'error',
-      'json/no-unsafe-values': 'error'
+      'json/no-unsafe-values': 'error',
+      'json/top-level-interop': 'error'
     }
   },
   {
