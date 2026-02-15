a3c831d834204772e15048b740d8e0d9ea40c3fe
Eric Cornelissen <ericornelissen@gmail.com>
2026-02-15 21:06:27 +0100

    Enable new `eslint-plugin-unicorn` rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -339,6 +339,7 @@ export default [
         'unicorn/explicit-length-check': 'error',
         'unicorn/filename-case': 'error',
         'unicorn/import-style': 'error',
+        'unicorn/isolated-functions': 'error',
         'unicorn/new-for-builtins': 'error',
         'unicorn/no-abusive-eslint-disable': 'error',
         'unicorn/no-accessor-recursion': 'error',
